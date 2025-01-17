eb13e680ef1f13bbf9e0ce8dd98cc6c82bce1fa6
BadaHong <badah@hanyang.ac.kr>
2025-01-17 20:13:23 +0900

    fix: slider

--- scripts.js
+++ scripts.js
@@ -23,12 +23,34 @@ function nextSlide() {
 }
 
 // Auto-advance slides every 5 seconds
-setInterval(nextSlide, 1300);
+setInterval(nextSlide, 1600);
 
 // Click handlers for navigation buttons
 navButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
         currentSlide = index;
         showSlide(currentSlide);
+    });
+});
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,            // Enables looping
+        margin: 270,           // Adds margin between items
+        nav: false,            // Shows navigation arrows
+        dots: true,           // Shows dots below the carousel
+        autoplay: true,       // Enables auto-slide
+        autoplayTimeout: 3000, // Auto-slide interval (in milliseconds)
+        responsive: {
+            0: {
+                items: 1      // 1 item for screens < 600px
+            },
+            600: {
+                items: 2      // 2 items for screens 600px - 1000px
+            },
+            1000: {
+                items: 3      // 3 items for screens > 1000px
+            }
+        }
     });
 });
